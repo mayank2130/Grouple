7b690d2cfea8d8d711ee7ac336980feb9ad8536e
mayank2130 <mayankthakur1712@gmail.com>
2024-09-03 00:42:38 +0530

    feat: login button

--- src/app/(landing)/_components/navbar/index.tsx
+++ src/app/(landing)/_components/navbar/index.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@/components/ui/button"
+import { Logout } from "@/icons"
+import Link from "next/link"
 import Menu from "./menu"
 
 type Props = {}
@@ -7,6 +10,17 @@ const LandingPageNavbar = (props: Props) => {
         <div className="w-full flex justify-center sticky top-0 items-center py-5 z-50">
             <p className="font-bold text-2xl">Grouple.</p>
             <Menu orientation="desktop" />
+            <div className="flex gap-2">
+                <Link href="/sign-in">
+                    <Button
+                        variant="outline"
+                        className="bg-themeBlack rounded-2xl flex gap-2 border-themeGray hover:bg-themeGray"
+                    >
+                        <Logout />
+                        Login
+                    </Button>
+                </Link>
+            </div>
         </div>
     )
 }
