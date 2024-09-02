438f4fa635315c82496e78139009d9f09cf9df4d
mayank2130 <mayankthakur1712@gmail.com>
2024-09-03 00:28:25 +0530

    feat: navbar items

---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+import { LANDING_PAGE_MENU, MenuProps } from "./menus"
+
+type GroupleConstantsProps = {
+    landingPageMenu: MenuProps[]
+}
+export const GROUPLE_CONSTANTS: GroupleConstantsProps = {
+    landingPageMenu: LANDING_PAGE_MENU,
+}
