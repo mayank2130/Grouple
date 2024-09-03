d971e580f298939098b19b0807e38a38f003ae85
mayank2130 <mayankthakur1712@gmail.com>
2024-09-03 22:15:55 +0530

    feat: forms

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,13 @@
+import { AuthFormProps, SIGN_IN_FORM, SIGN_UP_FORM } from "./forms"
 import { LANDING_PAGE_MENU, MenuProps } from "./menus"
 
 type GroupleConstantsProps = {
     landingPageMenu: MenuProps[]
+    signInForm: AuthFormProps[]
+    signUpForm: AuthFormProps[]
 }
 export const GROUPLE_CONSTANTS: GroupleConstantsProps = {
     landingPageMenu: LANDING_PAGE_MENU,
+    signUpForm: SIGN_UP_FORM,
+    signInForm: SIGN_IN_FORM,
 }
