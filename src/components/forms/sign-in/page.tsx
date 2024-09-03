91ec8e56b4c24af9ec0e6a5246e73fbd23d42f56
mayank2130 <mayankthakur1712@gmail.com>
2024-09-03 22:13:13 +0530

    feat: signin form

---
+++ src/components/forms/sign-in/page.tsx
@@ -0,0 +1,32 @@
+import { FormGenerator } from "@/components/global/form-generator"
+import { Loader } from "@/components/global/loader"
+import { Button } from "@/components/ui/button"
+import { GROUPLE_CONSTANTS } from "@/constants"
+import { useAuthSignIn } from "@/hooks/authentication"
+
+type Props = {}
+
+const SignInForm = (props: Props) => {
+    const { errors, isPending, onAuthenticateUser, register } = useAuthSignIn()
+
+    return (
+        <form
+            className="flex flex-col gap-3 mt-10"
+            onSubmit={onAuthenticateUser}
+        >
+            {GROUPLE_CONSTANTS.signInForm.map((field) => (
+                <FormGenerator
+                    {...field}
+                    key={field.id}
+                    register={register}
+                    errors={errors}
+                />
+            ))}
+            <Button type="submit" className="rounded-2xl">
+                <Loader loading={isPending}>Sign In with Email</Loader>
+            </Button>
+        </form>
+    )
+}
+
+export default SignInForm
