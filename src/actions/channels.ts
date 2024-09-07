faf57590a5411a4830a6e877c4ebcb5d23f183c7
mayank2130 <mayankthakur1712@gmail.com>
2024-09-07 22:54:59 +0530

    feat: create new channel action

--- src/actions/channels.ts
+++ src/actions/channels.ts
@@ -53,3 +53,44 @@ export const onGetChannelInfo = async (channelid: string) => {
         return { status: 400, message: "Oops! something went wrong" }
     }
 }
+
+export const onCreateNewChannel = async (
+    groupid: string,
+    data: {
+        id: string
+        name: string
+        icon: string
+    },
+) => {
+    try {
+        const channel = await client.group.update({
+            where: {
+                id: groupid,
+            },
+            data: {
+                channel: {
+                    create: {
+                        ...data,
+                    },
+                },
+            },
+            select: {
+                channel: true,
+            },
+        })
+
+        if (channel) {
+            return { status: 200, channel: channel.channel }
+        }
+
+        return {
+            status: 404,
+            message: "Channel could not be created",
+        }
+    } catch (error) {
+        return {
+            status: 400,
+            message: "Oops! something went wrong",
+        }
+    }
+}
