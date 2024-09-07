e2a46793541103fdc4570f83b479adc9ac5c58cf
mayank2130 <mayankthakur1712@gmail.com>
2024-09-07 23:43:38 +0530

    feat: menu icons

--- src/constants/menus.tsx
+++ src/constants/menus.tsx
@@ -1,4 +1,12 @@
-import { CreditCard, Explore, Home } from "@/icons"
+import {
+    AffiliateDuoToneBlack,
+    CreditCard,
+    Explore,
+    GlobeDuoToneBlack,
+    Home,
+    IDuotoneBlack,
+    ZapDouToneBlack,
+} from "@/icons"
 
 export type MenuProps = {
     id: number
@@ -29,5 +37,39 @@ export const LANDING_PAGE_MENU: MenuProps[] = [
         label: "Explore",
         icon: <Explore />,
         path: "/explore",
+    },
+]
+
+export const SIDEBAR_SETTINGS_MENU: MenuProps[] = [
+    {
+        id: 0,
+        label: "General",
+        icon: <IDuotoneBlack />,
+        path: "",
+    },
+    {
+        id: 1,
+        label: "Subscriptions",
+        icon: <CreditCard />,
+        path: "subscriptions",
+    },
+    {
+        id: 2,
+        label: "Affiliates",
+        icon: <AffiliateDuoToneBlack />,
+        path: "affiliates",
+    },
+    {
+        id: 3,
+        label: "Domain Config",
+        icon: <GlobeDuoToneBlack />,
+        path: "domains",
+    },
+    {
+        id: 4,
+        label: "Integration",
+        icon: <ZapDouToneBlack />,
+        path: "integrations",
+        integration: true,
     },
 ]
