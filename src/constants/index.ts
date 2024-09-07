ade4b923b0da85f24c2a84f3aee090e76b7fe65b
mayank2130 <mayankthakur1712@gmail.com>
2024-09-07 17:58:49 +0530

    feat: group list

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,16 +4,19 @@ import {
     CREATE_GROUP_PLACEHOLDER,
     CreateGroupPlaceholderProps,
 } from "./placeholder"
+import { GROUP_LIST, GroupListProps } from "./slider"
 
 type GroupleConstantsProps = {
     landingPageMenu: MenuProps[]
     signInForm: AuthFormProps[]
     signUpForm: AuthFormProps[]
     createGroupPlaceholder: CreateGroupPlaceholderProps[]
+    groupList: GroupListProps[]
 }
 export const GROUPLE_CONSTANTS: GroupleConstantsProps = {
     landingPageMenu: LANDING_PAGE_MENU,
     signUpForm: SIGN_UP_FORM,
     signInForm: SIGN_IN_FORM,
     createGroupPlaceholder: CREATE_GROUP_PLACEHOLDER,
+    groupList: GROUP_LIST,
 }
