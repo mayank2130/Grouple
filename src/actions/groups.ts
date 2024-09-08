f15f416be9077cf6b1c2dcb675b91783556011ba
mayank2130 <mayankthakur1712@gmail.com>
2024-09-08 13:39:11 +0530

    feat: join group action

--- src/actions/groups.ts
+++ src/actions/groups.ts
@@ -294,3 +294,26 @@ export const onSearchGroups = async (
         return { status: "400", message: "Oops! something went wrong" }
     }
 }
+
+export const onJoinGroup = async (groupid: string) => {
+    try {
+        const user = await onAuthenticatedUser()
+        const member = await client.group.update({
+            where: {
+                id: groupid,
+            },
+            data: {
+                member: {
+                    create: {
+                        userId: user.id,
+                    },
+                },
+            },
+        })
+        if (member) {
+            return { status: 200 }
+        }
+    } catch (error) {
+        return { status: 404 }
+    }
+}
